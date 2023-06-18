00c39dcd4874b3d6cb20d0f6415dd44c2c22aaef
Marcio Curvello <marciocurvello@gmail.com>
2023-06-18 11:12:53 -0300

    create model

---
+++ PersonalData/Controllers/PersonController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PersonalData.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PersonController : ControllerBase
+{
+    private readonly ILogger<PersonController> _logger;
+
+    public PersonController(ILogger<PersonController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet("sum/{firstNumber}/{secondNumber}")]
+    public IActionResult Get(string firstNumber, string secondNumber)
+    {
+        if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+        {
+            var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+            return Ok(sum.ToString());
+        }
+
+        return BadRequest("Invalid Input");
+    }
+
+    private bool IsNumeric(string strNumber)
+    {
+        bool isNumber = double.TryParse(strNumber, System.Globalization.NumberStyles.Any, System.Globalization.NumberFormatInfo.InvariantInfo, out double number);
+        return isNumber;
+    }
+
+    private decimal ConvertToDecimal(string strNumber)
+    {
+        decimal decimalValue;
+        if(decimal.TryParse(strNumber, out decimalValue))
+        {
+            return decimalValue;
+        }
+        return 0;
+    }
+
+    
+}
+
