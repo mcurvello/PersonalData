2957e727c00e164735a3943cdce428cbf9969e5f
Marcio Curvello <marciocurvello@gmail.com>
2023-06-18 11:26:56 -0300

    add service

---
+++ PersonalData/Services/IPersonService.cs
@@ -0,0 +1,13 @@
+﻿using PersonalData.Model;
+
+namespace PersonalData.Services
+{
+	public interface IPersonService
+	{
+		Person Create(Person person);
+		Person FindById(long id);
+		List<Person> FindAll();
+		Person Update(Person person);
+		void Delete(long id);
+	}
+}
---
+++ PersonalData/Services/Implementations/PersonService.cs
@@ -0,0 +1,69 @@
+﻿using PersonalData.Model;
+
+namespace PersonalData.Services.Implementations
+{
+	public class PersonService : IPersonService
+	{
+        private volatile int count;
+
+        public PersonService()
+		{
+		}
+
+        public Person Create(Person person)
+        {
+            return person;
+        }
+
+        public void Delete(long id)
+        {
+           
+        }
+
+        public List<Person> FindAll()
+        {
+            List<Person> people = new List<Person>();
+            for (int i = 0; i < 8; i++)
+            {
+                Person person = MockPerson(i);
+                people.Add(person);
+            }
+            return people;
+        }
+
+        
+        public Person FindById(long id)
+        {
+            return new Person
+            {
+                Id = IncrementAndGet(),
+                FirstName = "Marcio",
+                LastName = "Curvello",
+                Address = "São Paulo - São Paulo - Brasil",
+                Gender = "Male"
+            };
+        }
+
+        public Person Update(Person person)
+        {
+            return person;
+        }
+
+        private Person MockPerson(int i)
+        {
+            return new Person
+            {
+                Id = IncrementAndGet(),
+                FirstName = "Person Name" + i,
+                LastName = "Person LastName" + i,
+                Address = "Some Address" + i,
+                Gender = "Some Gender"
+            };
+        }
+
+        private long IncrementAndGet()
+        {
+            return Interlocked.Increment(ref count);
+        }
+    }
+}
